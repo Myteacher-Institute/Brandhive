cfe1c956c8bef35068295eed6d5c4a305351be22
Rapuluchukwu <adrapulu@gmail.com>
2025-05-16 11:24:41 +0100

    styled the play-pause button for the why choose us video

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,37 @@
-      function openOverlay(imageSrc) {
-            const overlay = document.getElementById('imageOverlay');
-            const image = document.getElementById('overlayImage');
+function openOverlay(imageSrc) {
+    const overlay = document.getElementById('imageOverlay');
+    const image = document.getElementById('overlayImage');
 
-            image.src = imageSrc;
-            overlay.style.display = 'flex';
-            document.body.style.overflow = 'hidden'; // Prevent scrolling
-        } 8
+    image.src = imageSrc;
+    overlay.style.display = 'flex';
+    document.body.style.overflow = 'hidden'; // Prevent scrolling
+} 8
 
-        function closeOverlay() {
-            const overlay = document.getElementById('imageOverlay');
-            overlay.style.display = 'none';
-            document.body.style.overflow = 'auto'; // Enable scrolling again
-        }
+function closeOverlay() {
+    const overlay = document.getElementById('imageOverlay');
+    overlay.style.display = 'none';
+    document.body.style.overflow = 'auto'; // Enable scrolling again
+}
 
-        // Close overlay when clicking outside the image
-        document.getElementById('imageOverlay').addEventListener('click', function (e) {
-            if (e.target === this) {
-                closeOverlay();
-            }
-        });
+// Close overlay when clicking outside the image
+document.getElementById('imageOverlay').addEventListener('click', function (e) {
+    if (e.target === this) {
+        closeOverlay();
+    }
+});
 
+const video = document.getElementById('video');
+const btn = document.getElementById('playPauseBtn');
+const icon = btn.querySelector('i');
 
-        
+btn.addEventListener('click', () => {
+    if (video.paused) {
+        video.play();
+        icon.classList.remove('fa-play');
+        icon.classList.add('fa-pause');
+    } else {
+        video.pause();
+        icon.classList.remove('fa-pause');
+        icon.classList.add('fa-play');
+    }
+});
